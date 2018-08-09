f8fb78eda258a7a42644199e463cc881854928b8
Chuck D'Antonio <c_dantonio@acm.org>
2018-08-09 14:02:54 -0400

    Use JSO instead of manual

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { JSO, Popup } from 'jso';
 
 class ExampleApplication extends Component {
 
   constructor(props) {
     super(props)
-    console.log(process.env.VCAP_SERVICES)
-    console.log(process.env.VCAP_APPLICATION)
+
     if ( process.env.VCAP_SERVICES && process.env.VCAP_APPLICATION) {
       let vcap_services = JSON.parse(process.env.VCAP_SERVICES) ;
       let vcap_application = JSON.parse(process.env.VCAP_APPLICATION) ;
 
       if ( ( vcap_services["p-identity"]) && ( vcap_services["p-identity"][0] ) && ( vcap_services["p-identity"][0].credentials ) ) {
         this.ssoServiceUrl = vcap_services["p-identity"][0].credentials.auth_domain;
-        this.clientId = vcap_services["p-identity"][0].credentials.client_id ;
-        this.authDomain = vcap_services["p-identity"][0].credentials.auth_domain ;
-        this.baseUrl = vcap_application && vcap_application.uris && ("https://" + vcap_application.uris[0] + "/") ;
+        this.signon = new JSO({
+          providerID: "pivotal",
+          client_id: vcap_services["p-identity"][0].credentials.client_id ,
+          redirect_uri: vcap_application && vcap_application.uris && ("https://" + vcap_application.uris[0] + "/implicit"),
+          authorization: this.ssoServiceUrl + "/oauth/authorize",
+          scopes: { request: ["openid"] }
+        });
+      } else {
+        this.clientId = "client_id_placeholder";
       }
-    } else {
-      this.clientId = "client_id_placeholder"
+    }
+
+    this.state = {
+      isLoaded: false,
+      token: ""
     }
   }
 
@@ -34,8 +43,18 @@ class ExampleApplication extends Component {
     return JSON.stringify(JSON.parse(atob(token.split('\.')[1])), null, '  ');
   }
 
-  constructUaaRedirect() {
-    return `${this.ssoServiceUrl}/oauth/authorize?client_id=${this.clientId}&response_type=token+id_token&redirect_uri=${this.baseUrl}implicit#uaaLocation=${this.ssoServiceUrl}`
+  componentDidMount() {
+    if (window.location.pathname === "/implicit") {
+        this.signon.callback();
+        this.signon.getToken()
+          .then( (token) => {
+            console.log("I got the token: " + token)
+            this.setState({
+              isLoaded: true,
+              token: token.access_token
+            });
+          });
+    }
   }
 
   render() {
@@ -49,7 +68,7 @@ class ExampleApplication extends Component {
           </div>
           )
       } else {
-        var href = this.constructUaaRedirect();
+        var href="/implicit";
         page = (<div>
           <h1>Implicit sample</h1>
           <h2>What do you want to do?</h2>
@@ -61,44 +80,47 @@ class ExampleApplication extends Component {
         </div>)
       }
     }
+
     if (window.location.pathname === "/implicit") {
-      var expiresIn = this.getFragment("expires_in");
-      var scope = this.getFragment("scope");
-      var jti = this.getFragment("jti");
-      var idToken = this.prettyToken(this.getFragment("id_token"));
-      var token = this.prettyToken(this.getFragment("access_token"));
-      var tokenType = this.getFragment("token_type");
-      var profileUrl = this.ssoServiceUrl + '/profile';
-      const urlStr = window.location.protocol + '//' + window.location.host;
-      var logoutUrl = this.ssoServiceUrl + '/logout.do' + '?redirect=' + urlStr + '&client_id=' + this.clientId;
-      page = (<div>
-        <h1>Implicit sample</h1>
-        <p>The server only saw a request for /implicit. Everything after the # in the address bar is stuff that only your browser can see.</p>
-        <p>Your ID Token token is:</p>
-        <pre id="id_token">{idToken}</pre>
-        <p>Expires in:</p>
-        <pre id="expires_in">{expiresIn}</pre>
-        <p>Scope:</p>
-        <pre id="scope">{scope}</pre>
-        <p>JTI:</p>
-        <pre id="jti">{jti}</pre>
+      let { isLoaded, token } = this.state
 
-        <p>Your access token is:</p>
-        <pre id="token">{token}</pre>
-        <p>Your access token type:</p>
-        <pre id="token_type">{tokenType}</pre>
+      if ( isLoaded ) {
+          var profileUrl = this.ssoServiceUrl + '/profile';
+          const urlStr = window.location.protocol + '//' + window.location.host;
+          var logoutUrl = this.ssoServiceUrl + '/logout.do' + '?redirect=' + urlStr + '&client_id=' + this.clientId;
+          var productsUrl = "/products"
 
-        <h2>What do you want to do?</h2>
-        <ul>
-          <li>
-            <a id="profile" target="uaa" href={profileUrl}>See your account profile on UAA (so you can de-authorize this client)</a>
-          </li>
-          <li>
-            <a id="logout" href={logoutUrl}>Log out of UAA</a>
-          </li>
-        </ul>
-      </div>);
+          page = (<div>
+            <h1>Implicit sample</h1>
+            <p>Your access token is:</p>
+            <pre id="token">{token}</pre>
+
+            <h2>What do you want to do?</h2>
+            <ul>
+              <li>
+                <a id="profile" target="uaa" href={productsUrl}>Use the token to call a service</a>
+              </li>
+              <li>
+                <a id="profile" target="uaa" href={profileUrl}>See your account profile on UAA (so you can de-authorize this client)</a>
+              </li>
+              <li>
+                <a id="logout" href={logoutUrl}>Log out of UAA</a>
+              </li>
+            </ul>
+          </div>);
+          console.log("page set");
+      } else {
+        page = "<div>Logging in...</div>"
+      }
+
     }
+
+    if ( window.location.pathname == "/products") {
+      var serviceUrl = "https://core-cf-webapi-comedic-eland.homelab.fynesy.com"
+      page = ( <div>Product component here</div> ) ;
+    }
+
+    console.log("returning page")
     return page;
   }
 }
